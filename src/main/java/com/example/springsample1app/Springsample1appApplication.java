63f4fc9e279a1878e3b17e09eccbea7627e94e98
Yuta <ezochi66@gmail.com>
2024-01-28 22:56:14 +0900

    HTMLのコードを出力"

--- src/main/java/com/example/springsample1app/Springsample1appApplication.java
+++ src/main/java/com/example/springsample1app/Springsample1appApplication.java
@@ -2,66 +2,38 @@ package com.example.springsample1app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @RestController
 public class Springsample1appApplication {
 
-	DataObject[] data = {
-			new DataObject( "noname", "no email address", 0 ),
-			new DataObject( "taro", "taro@yamada", 39 ),
-			new DataObject( "hanako", "hanako@flower", 28 ),
-			new DataObject( "sachiko", "sachiko@happy", 17 ),
-			new DataObject( "jiro", "jiro@change", 6 )
-	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springsample1appApplication.class, args);
 	}
 
-	@RequestMapping("/{num}")
-	public DataObject index(@PathVariable int num) {
-		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
-		return data[n];
-	}
-}
-
-class DataObject {
-	private String name;
-	private String mail;
-	private int age;
-
-	public DataObject(String name, String mail, int age) {
-		super();
-		this.name = name;
-		this.mail = mail;
-		this.age = age;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-	
-	public String getMail() {
-		return this.mail;
-	}
-
-	public int getAge() {
-		return this.age;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
+	@RequestMapping("/")
+	public String index(
+		HttpServletRequest request,
+		HttpServletResponse response) {
+		response.setContentType(MediaType.TEXT_HTML_VALUE);
+			String content = """
+			<!DOCTYPE html>
+				<head>
+					<title>Sample App</title>
+				</head>
+				<body>
+					<h1>Sample App</h1>
+					<p>This is sample app page!</p>
+				</body>
+			</html>
+			""";
+		return content;
 	}
 }
