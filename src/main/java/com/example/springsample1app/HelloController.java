46c98a8d7014a1f722ce2e0a2dcc59b0a953c3d2
Yuta <ezochi66@gmail.com>
2024-01-29 23:20:03 +0900

    ControllerでModelを用意

--- src/main/java/com/example/springsample1app/HelloController.java
+++ src/main/java/com/example/springsample1app/HelloController.java
@@ -1,18 +1,14 @@
 package com.example.springsample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/{temp}")
-    public String index(@PathVariable String temp) {
-        switch (temp) {
-            case "index":
-                return "index";
-            default:
-                return "other";
-        }
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg", "これはコントローラーに用意したサンプルです。");
+        return "index";
     }
 }
