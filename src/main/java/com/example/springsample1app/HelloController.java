9b0f5282ca8fcf09b194e2ad6578c4451b7994f3
Yuta <ezochi66@gmail.com>
2024-01-29 23:22:20 +0900

    パラメータを利用

--- src/main/java/com/example/springsample1app/HelloController.java
+++ src/main/java/com/example/springsample1app/HelloController.java
@@ -2,13 +2,19 @@ package com.example.springsample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String index(Model model) {
-        model.addAttribute("msg", "これはコントローラーに用意したサンプルです。");
+    @RequestMapping("/{num}")
+    public String index(@PathVariable int num, Model model) {
+        int res = 0;
+        for (int i = 1; i <= num; i++) {
+            res += i;
+        }
+        
+        model.addAttribute("msg", "total:" + res);
         return "index";
     }
 }
