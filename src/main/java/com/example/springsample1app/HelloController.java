cec81f5641f58b5de13df4a698bfd16f6644f2af
Yuta <ezochi66@gmail.com>
2024-02-01 15:41:35 +0900

    th:switchによりスイッチ処理

--- src/main/java/com/example/springsample1app/HelloController.java
+++ src/main/java/com/example/springsample1app/HelloController.java
@@ -1,18 +1,17 @@
 package com.example.springsample1app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping(value="/", method = RequestMethod.GET)
-    public ModelAndView index(ModelAndView mav) {
+    @RequestMapping("/{month}")
+    public ModelAndView index(@PathVariable int month, ModelAndView mav) {
         mav.setViewName("index");
-        mav.addObject("msg", "データを表示します。");
-        String[] data = new String[] { "One", "Two", "Three" };
-        mav.addObject("data", data);
+        mav.addObject("msg", month + " 月は？");
+        mav.addObject("month", month);
         return mav;
     }
 }
