705a82c817a5cf17a638335bf7e88e9f2e9f0c58
Yuta <ezochi66@gmail.com>
2024-02-02 15:12:13 +0900

    [Groovy Template]繰り返し

--- src/main/java/com/example/springsample1app/HelloController.java
+++ src/main/java/com/example/springsample1app/HelloController.java
@@ -5,14 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
-    private boolean flag = false;
     @RequestMapping("/")
     public ModelAndView index(ModelAndView mav) {
-        flag = !flag;
+        String[] data = {"Windows", "macOS", "Linux", "ChromeOS"};
         mav.setViewName("index");
         mav.addObject("title", "Groovy templates");
         mav.addObject("msg", "This is sample message!!");
-        mav.addObject("flag", flag);
+        mav.addObject("data", data);
         return mav;
     }
 }
