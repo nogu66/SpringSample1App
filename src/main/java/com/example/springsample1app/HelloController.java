454771bc284590e541effeb1313fed7178a49a27
Yuta <ezochi66@gmail.com>
2024-02-02 14:14:28 +0900

    Mustacheテンプレートエンジンの導入

--- src/main/java/com/example/springsample1app/HelloController.java
+++ src/main/java/com/example/springsample1app/HelloController.java
@@ -1,17 +1,15 @@
 package com.example.springsample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/{month}")
-    public ModelAndView index(@PathVariable int month, ModelAndView mav) {
+    @RequestMapping("/")
+    public ModelAndView index(ModelAndView mav) {
         mav.setViewName("index");
-        mav.addObject("msg", month + " 月は？");
-        mav.addObject("month", month);
+        mav.addObject("msg", "メッセージだよ");
         return mav;
     }
 }
