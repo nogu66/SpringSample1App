3ab9ea8bc0d444d2218bb4e6a9c48d5a91dd6b77
Yuta <ezochi66@gmail.com>
2024-02-02 15:09:36 +0900

    [Groovy Template]条件分岐

--- src/main/java/com/example/springsample1app/HelloController.java
+++ src/main/java/com/example/springsample1app/HelloController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
+    private boolean flag = false;
     @RequestMapping("/")
     public ModelAndView index(ModelAndView mav) {
+        flag = !flag;
         mav.setViewName("index");
         mav.addObject("title", "Groovy templates");
         mav.addObject("msg", "This is sample message!!");
+        mav.addObject("flag", flag);
         return mav;
     }
 }
--- src/main/resources/templates/index.tpl
+++ src/main/resources/templates/index.tpl
@@ -7,8 +7,15 @@ html(lang:'ja') {
         link(rel:"stylesheet", type:"text/css", href:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css")
     }
 
-    body {
+    body(class:"container") {
         h1(class:"display-4", title)
-        p(msg)
+        p("Flage: $flag.");
+        if(flag) {
+            div(class:"alert alert-success") {
+                h6(msg)
+            }
+        } else {
+            p("no message...ç")
+        }
     }
 }
