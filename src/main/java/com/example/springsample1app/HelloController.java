6be2b959127db8cad81e47cb9ab7eb798163fcd3
Yuta <ezochi66@gmail.com>
2024-02-02 14:25:50 +0900

    [Mustache]条件いよる分岐

--- src/main/java/com/example/springsample1app/HelloController.java
+++ src/main/java/com/example/springsample1app/HelloController.java
@@ -6,16 +6,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+    private boolean flag = false;
     @RequestMapping("/")
     public ModelAndView index(ModelAndView mav) {
-        String msg = """
-                <div class="border boeder-primary">
-                <h2>メッセージ</h2>
-                <p>THis is sample message!</p>
-                </div>
-                """;
         mav.setViewName("index");
-        mav.addObject("msg", msg);
+        flag = !flag;
+        mav.addObject("flag", flag);
+        mav.addObject("msg", "メッセージを表示します。");
         return mav;
     }
 }
