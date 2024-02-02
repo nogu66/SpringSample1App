fc44e581d8853b90fab97c83383c4307ea24bda8
Yuta <ezochi66@gmail.com>
2024-02-02 14:36:40 +0900

    [Mustache]繰り返し

--- src/main/java/com/example/springsample1app/HelloController.java
+++ src/main/java/com/example/springsample1app/HelloController.java
@@ -10,9 +10,26 @@ public class HelloController {
     @RequestMapping("/")
     public ModelAndView index(ModelAndView mav) {
         mav.setViewName("index");
-        flag = !flag;
-        mav.addObject("flag", flag);
-        mav.addObject("msg", "メッセージを表示します。");
+        MyData[] data = new MyData[] {
+            new MyData("Taro", 39),
+            new MyData("Hanako", 28),
+            new MyData("Sachiko", 17),
+        };
+        mav.addObject("data", data);
         return mav;
+    }
+}
+
+class MyData {
+    public String name;
+    public int age;
+
+    public MyData(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return String.format("{Name: %s, age: %s}", name, age);
     }
 }
