02f06e770ae98f33d778b5b1385aa16c7e0f85b5
Yuta <ezochi66@gmail.com>
2024-02-05 15:07:19 +0900

    クレイアノテーションでの検索

--- src/main/java/com/example/springsample1app/Person.java
+++ src/main/java/com/example/springsample1app/Person.java
@@ -5,15 +5,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;;
 
 @Entity
 @Table(name = "people")
+@NamedQueries(
+    @NamedQuery(
+        name="findWithName",
+        query="from Person where name like :fname"
+    )
+)
 public class Person {
     
     @Id
--- src/main/java/com/example/springsample1app/PersonDAOPersonalImpl.java
+++ src/main/java/com/example/springsample1app/PersonDAOPersonalImpl.java
@@ -42,14 +42,7 @@ public class PersonDAOPersonalImpl implements PersonDAO<Person> {
     @Override
     public List<Person> find(String fstr) {
         List<Person> list = null;
-        String qstr = "from Person where id =  :fid or name like :fname or mail like :fmail";
-        Long fid = 0L;
-        try {
-            fid = Long.parseLong(fstr);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-        Query query = entityManager.createQuery(qstr).setParameter("fid", fid).setParameter("fname", "%" + fstr + "%").setParameter("fmail", fstr+ "%@%");
+        Query query = entityManager.createNamedQuery("findWithName").setParameter("fname", "%"+fstr+"%");
         list = query.getResultList();
         return list;
     }
