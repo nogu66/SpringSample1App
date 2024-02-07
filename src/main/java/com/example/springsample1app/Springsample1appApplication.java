a96aef02483d971e18016a025ec937100b93fe34
Yuta <ezochi66@gmail.com>
2024-02-07 11:19:54 +0900

    構成クラスの作成

--- src/main/java/com/example/springsample1app/Springsample1appApplication.java
+++ src/main/java/com/example/springsample1app/Springsample1appApplication.java
@@ -2,9 +2,6 @@ package com.example.springsample1app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Springsample1appApplication {
@@ -12,15 +9,5 @@ public class Springsample1appApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springsample1appApplication.class, args);
-	}
-
-	@Bean
-	public Post post() {
-		return new Post(0, 0, "Dummy", "This is dummy post.");
-	}
-
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
 	}
 }
